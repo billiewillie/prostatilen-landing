5bd59acf4a7f63d676145153249e7cbdcce63b21
vadimbelinovich <vadimbelinovich@gmail.com>
2019-11-18 15:27:04 +0300

    edited

--- src/js/component/slider.js
+++ src/js/component/slider.js
@@ -2,11 +2,12 @@ import "slick-carousel";
 
 function slider() {
   $('ul.slider').slick({
-    autoplay: false,
     dots: true,
     arrows: false,
-    slidesToShow: 1
+    slidesToShow: 1,
+    autoplay: false,
+    autoplaySpeed: 3000,
   });
-}
+};
 
 export default slider();
